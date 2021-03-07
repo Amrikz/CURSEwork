f5fdff6480a615c2afab4cbf4fbf56f2a72b1f93
Rikz <rikzforjob@gmail.com>
2021-03-07 10:28:00 +0600

    Small Request update

--- app/Lib/Request/Request.php
+++ app/Lib/Request/Request.php
@@ -8,6 +8,13 @@ class Request
 {
     public static $request;
 
+
+    private static function _override()
+    {
+        if (self::$request && !empty(self::$request)) $_POST = self::$request;
+    }
+
+
     public static function get($override_post = false)
     {
         if (!self::$request)
@@ -15,7 +22,7 @@ class Request
             $request = json_decode(file_get_contents('php://input'), true);
             self::$request = $request;
         }
-        if ($override_post && self::$request && !empty(self::$request)) $_POST = self::$request;
+        if ($override_post) self::_override();
         return self::$request;
     }
 }
