cb9c631e088dc06e446e1caf9b7b69b8211f01b3
Rikz <rikzforjob@gmail.com>
2021-04-06 11:10:34 +0600

    Another try of commit fix

---
+++ App/Controllers/AdminController.class.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Controllers;
+
+
+use App\Models\Roles;
+use App\Models\Users;
+
+class AdminController extends BaseController
+{
+    public function __construct()
+    {
+        if (!$_SESSION['user'])
+        {
+            header('Location: '.url_addition());
+        }
+        else
+        {
+            $user = Users::GetByID($_SESSION['user']['id']);
+            $role = Roles::GetByID($user['role_id']);
+            if (!$role || $role['power'] < Roles::$admin_power_level) header('Location: '.url_addition());
+        }
+
+        parent::__construct();
+    }
+
+
+    public function index()
+    {
+
+    }
+}
